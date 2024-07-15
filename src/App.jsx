98e76cfab0d1baf78beda04cf48f236f2d964e81
Anurag Kundu <anuragkundu064@gmail.com>
2024-07-15 21:41:22 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  const [calculatedValue , setValue] = useState('');
+  const stylebutton = {
+    backgroundColor:"rgb(164, 159, 159)"
+  }
+
+  const showInput = (charact)=>{
+    let string = calculatedValue+charact;
+    setValue(string);
+  }
+  const handleResult =()=>{
+    let myres = eval(calculatedValue);
+    setValue(myres);
+  }
+
+
+  return (
+    <>
+      <div className="container">
+        <div className="calculator">
+          <input type='text' value={calculatedValue} disabled placeholder='0'/>
+          <div className="all-buttons">
+          <button className='ac-class' style={{backgroundColor:"darkred"}} onClick={()=>setValue("")} >AC</button>
+          <button style={stylebutton} onClick={()=>showInput("/")}>/</button>
+          <button style={stylebutton} onClick={()=>showInput("*")}>*</button>
+          <button onClick={()=>showInput(5)}>5</button>
+          <button onClick={()=>showInput(2)}>2</button>
+          <button onClick={()=>showInput(1)}>1</button>
+          <button style={stylebutton} onClick={()=>showInput("-")}>-</button>
+          <button onClick={()=>showInput(3)}>3</button>
+          <button onClick={()=>showInput(8)}>8</button>
+          <button onClick={()=>showInput(9)}>9</button>
+          <button style={stylebutton} onClick={()=>showInput("+")}>+</button>
+          <button onClick={()=>showInput(4)}>4</button>
+          <button onClick={()=>showInput(6)}>6</button>
+          <button onClick={()=>showInput(7)}>7</button>
+          <button className='zero-class' onClick={()=>showInput(0)}>0</button>
+          <button onClick={()=>showInput('.')}>.</button>
+          <button className='equal-class' onClick={handleResult}>=</button>
+
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
